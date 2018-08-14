d05d546a1b0e6375360c364c0bc1928218310c87
Giovanni Sakti <giosakti@gmail.com>
2018-08-14 15:44:59 +0700

    [gio/tara] parameterize ruby path, gems path and fluentd version

--- resources/setup.rb
+++ resources/setup.rb
@@ -1,12 +1,15 @@
-property :logging_name,   String, name_property: true
-property :group,          String, default: 'fluentd'
-property :user,           String, default: 'fluentd'
-property :config_dir,     String, required: true
-property :log_file_path,  String, required: true
-property :app_secret,     String, required: true
-property :produce_url,    String, required: true
-property :process_group,  String
-property :process_user,   String
+property :logging_name,     String, name_property: true
+property :group,            String, default: 'fluentd'
+property :user,             String, default: 'fluentd'
+property :config_dir,       String, required: true
+property :log_file_path,    String, required: true
+property :app_secret,       String, required: true
+property :produce_url,      String, required: true
+property :fluentd_version,  String, default: '1.2.4'
+property :gem_path,         String, default: '/opt/chef/embedded/lib/ruby/gems/2.5.0/gems'
+property :ruby_binary_path, String, default: '/opt/chef/embedded/bin'
+property :process_group,    String
+property :process_user,     String
 
 default_action :install
 
@@ -33,6 +36,9 @@ end
 action :package_install do
   apt_update
   package %w(build-essential zlib1g-dev)
+  gem_package 'fluentd' do
+    version new_resource.fluentd_version
+  end
   gem_package 'fluent-plugin-barito'
 end
 
@@ -55,7 +61,7 @@ action :configure do
               app_secret: new_resource.app_secret,
               produce_url: new_resource.produce_url
     )
-    notifies :reload, "barito_agent_setup[#{new_resource.logging_name}]", :delayed
+    notifies :restart, "barito_agent_setup[#{new_resource.logging_name}]", :delayed
   end
 end
 
@@ -63,10 +69,10 @@ action :systemd do
   # Configure systemd unit with options
   unit = node[cookbook_name]['systemd_unit'].to_hash
   conf = "#{new_resource.config_dir}/#{new_resource.logging_name}.conf"
-  bin = "/opt/chef/embedded/lib/ruby/gems/2.5.0/gems/fluentd-0.14.25/bin/fluentd"
+  bin = "#{new_resource.gem_path}/fluentd-#{new_resource.fluentd_version}/bin/fluentd"
   unit['Service']['User'] = new_resource.process_user || new_resource.user 
   unit['Service']['Group'] =  new_resource.process_group || new_resource.group
-  unit['Service']['ExecStart'] = "/bin/bash -lc 'PATH=$PATH:/opt/chef/embedded/bin #{bin} -c #{conf}'"
+  unit['Service']['ExecStart'] = "/bin/bash -lc 'PATH=#{new_resource.ruby_binary_path}:$PATH #{bin} -c #{conf}'"
 
   systemd_unit "fluentd-#{new_resource.logging_name}.service" do
     enabled true
@@ -80,16 +86,9 @@ action :systemd do
   end
 end
 
-action :reload do
-  service "fluentd-#{new_resource.logging_name}.service" do
-    action [:enable, :reload]
-    supports status: true, start: true, stop: true, restart: true, reload: true
-  end
-end
-
 action :restart do
   service "fluentd-#{new_resource.logging_name}.service" do
     action [:enable, :restart]
-    supports status: true, start: true, stop: true, restart: true, reload: true
+    supports status: true, start: true, stop: true, restart: true
   end
 end
