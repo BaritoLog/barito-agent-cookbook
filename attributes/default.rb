b7f8d41c6a23f9fb094948085f26ea777ff8f7a0
opan <opan.neutron@gmail.com>
2018-08-16 17:13:06 +0700

    [opan/bherly] Fix and Allow td-agent to read journalctl and systemctl

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,14 +8,43 @@
 
 cookbook_name = 'barito-agent'
 
-default[cookbook_name]['produce_url'] = 'https://barito-router.golabs.io/produce'
-default[cookbook_name]['sources'] = []
-default[cookbook_name]['matches'] = []
+default[cookbook_name]['sources'] = [
+  {
+    type: 'systemd',
+    name: 'barito-journalctl',
+    path: '/run/log/journal',
+    read_from_head: true,
+    storage: {
+      '@type': 'local',
+      persistent: true,
+      path: '/etc/td-agent/barito-journalctl.pos'
+    },
+    entry: {
+      fields_strip_underscores: true,
+      fields_lowercase: true
+    }
+  }
+]
+
+default[cookbook_name]['matches'] = [
+  {
+    type: 'barito_vm',
+    name: 'barito-journalctl',
+    application_secret: '123456-secret',
+    produce_url: 'http://barito-producer',
+    buffer: {
+      flush_mode: 'immediate'
+    }
+  }
+]
 
 ## td-agent specific configuration
 default[:td_agent][:version] = '3'
 default[:td_agent][:plugins] = [
+  'systemd',
   {"barito" => { "version" => "0.1.8"}}
 ]
+# default[:td_agent][:uid] = 'root'
+# default[:td_agent][:gid] = 'root'
 default[:td_agent][:includes] = true
 default[:td_agent][:default_config] = false
--- recipes/default.rb
+++ recipes/default.rb
@@ -11,14 +11,23 @@ package %w(build-essential zlib1g-dev)
 
 include_recipe 'td-agent'
 
+
 node[cookbook_name]['sources'].each do |source|
+  if source['type'] == 'systemd'
+    execute 'Change group ownership of journalctl directory' do
+      command 'sudo chown -R :systemd-journal /run/log/journal'
+    end
+
+    execute 'Add td-agent to systemd-journal group' do
+      command 'sudo usermod -a -G systemd-journal td-agent'
+    end
+  end
+
   td_agent_source "source-#{source['name']}" do
     type source['type']
     tag source['name']
-    parameters(
-      path: source['path'],
-      parse: {'@type' => 'none'}
-    )
+    parameters source
+    action :create
   end
 end
 
@@ -26,10 +35,11 @@ node[cookbook_name]['matches'].each do |match|
   td_agent_match "match-#{match['name']}" do
     type match['type']
     tag match['name']
-    parameters(
-      application_secret: match['app_secret'],
-      produce_url: node[cookbook_name]['produce_url'],
-      buffer: {flush_mode: 'immediate'}
-    )
+    parameters match
+    action :create
   end
+end
+
+execute 'Reload td-agent and systemctl' do
+  command 'sudo systemctl daemon-reload && sudo systemctl restart td-agent'
 end
