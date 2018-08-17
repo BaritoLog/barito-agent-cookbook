8696ef813c6edb75749ce66f19d54590e81c594f
opan <opan.neutron@gmail.com>
2018-08-17 17:53:35 +0700

    [opan] Remove default value for sources and matches attributes and move it to readme

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
 # barito-agent-cookbook
 
 Chef cookbook to setup Barito Agent on our nodes.
+
+## Using `fluent-plugin-systemd`
+
+Below is the examples of input configuration.
+
+```ruby
+default[cookbook_name]['sources'] = [
+  {
+    type: 'systemd',
+    name: 'barito-journalctl',
+    path: '/run/log/journal',
+    raw_options: ['matches'],
+    matches: [{'_SYSTEMD_UNIT': 'syslog.service'}],
+    read_from_head: true,
+    storage: {
+      '@type': 'local',
+      persistent: true,
+      path: '/etc/td-agent/barito-journalctl.pos'
+    },
+    entry: {
+      fields_strip_underscores: true,
+      fields_lowercase: true
+    }
+  }
+]
+```
+
--- attributes/default.rb
+++ attributes/default.rb
@@ -8,37 +8,8 @@
 
 cookbook_name = 'barito-agent'
 
-default[cookbook_name]['sources'] = [
-  {
-    type: 'systemd',
-    name: 'barito-journalctl',
-    path: '/run/log/journal',
-    raw_options: ['matches'],
-    matches: [{'_SYSTEMD_UNIT': 'syslog.service'}],
-    read_from_head: true,
-    storage: {
-      '@type': 'local',
-      persistent: true,
-      path: '/etc/td-agent/barito-journalctl.pos'
-    },
-    entry: {
-      fields_strip_underscores: true,
-      fields_lowercase: true
-    }
-  }
-]
-
-default[cookbook_name]['matches'] = [
-  {
-    type: 'barito_vm',
-    name: 'barito-journalctl',
-    application_secret: '123456-secret',
-    produce_url: 'http://barito-producer',
-    buffer: {
-      flush_mode: 'immediate'
-    }
-  }
-]
+default[cookbook_name]['sources'] = []
+default[cookbook_name]['matches'] = []
 
 ## td-agent specific configuration
 default[:td_agent][:version] = '3'
