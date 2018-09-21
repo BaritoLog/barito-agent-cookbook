9600947e59e62de3e8da00f636ac81461166e239
Giovanni Sakti <giosakti@gmail.com>
2018-09-21 19:13:49 +0700

    update barito fluent plugin gem version

--- attributes/default.rb
+++ attributes/default.rb
@@ -16,7 +16,7 @@ default[cookbook_name]['groups'] = []
 default[:td_agent][:version] = '3'
 default[:td_agent][:plugins] = [
   'systemd',
-  {"barito" => { "version" => "0.1.10"}}
+  {"barito" => { "version" => "0.1.13"}}
 ]
 default[:td_agent][:includes] = true
 default[:td_agent][:default_config] = false
