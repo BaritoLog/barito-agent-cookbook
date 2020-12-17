5fd50870e5c2c9df007bed9d11c66b45cec79c53
Galuh Estya <galuhestya@gmail.com>
2020-12-17 17:02:17 +0700

    differentiate the name of filter config file from the

--- resources/filter.rb
+++ resources/filter.rb
@@ -6,6 +6,7 @@ default_action :config
 
 action :config do
   td_agent_filter "#{new_resource.name}" do
+    filter_name "filter-#{new_resource.name}"
     type new_resource.type
     tag new_resource.name
     parameters new_resource.parameters
