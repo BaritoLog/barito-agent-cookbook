52ca5891d7dc1e5e0a6f895f674a87bf4e2313b1
Fadli Nurhasan <fadli.nurhasan@gmail.com>
2021-09-22 11:36:02 +0700

    add filter name attr

--- resources/filter.rb
+++ resources/filter.rb
@@ -1,12 +1,15 @@
 property :name, String, name_property: true
 property :type, String, required: true
 property :parameters, Hash, default: {}
+property :filter_name, String
 
 default_action :config
 
 action :config do
+  filter_name = new_resource.filter_name || new_resource.name
+
   td_agent_filter "#{new_resource.name}" do
-    filter_name "filter-#{new_resource.name}"
+    filter_name "filter-#{filter_name}"
     type new_resource.type
     tag new_resource.name
     parameters new_resource.parameters
